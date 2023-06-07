1791d003dc105c080fb7ee767fac5e52b3165ea9
SanjanaR24 <sanjana.reddy2403@gmail.com>
2023-06-07 15:14:00 -0500

    commit

--- CoreJava/src/Week2/arrayRotation.java
+++ CoreJava/src/Week2/arrayRotation.java
@@ -2,17 +2,17 @@ package Week2;
         public class arrayRotation{
             public static void main(String[] args) {
                     int array[]=new int[]{1,3,45,78,32,89};
-                int k=2;
+                    int k=2;
                         int n=array.length;
-                        int b[]=new int[k];
+                        int temp[]=new int[k];
                         for(int i=0;i<k;i++){
-                            b[i]=array[i];
+                            temp[i]=array[i];
                         }
                         for(int i=0;i<(n-k);i++){
                             array[i]=array[k+i];
                         }
                         for(int i=n-k;i<n;i++){
-                            array[i]=b[i-(n-k)];
+                            array[i]=temp[i-(n-k)];
                         }
                         for(int i=0;i<array.length;i++){
                         System.out.println(array[i]);
