1daf9a946b0e5dd02f9331780fac0bc702b8c7ad
SanjanaR24 <sanjana.reddy2403@gmail.com>
2023-06-12 14:32:44 -0500

    commit

--- CoreJava/src/Week2/DisplayCsvFile.java
+++ CoreJava/src/Week2/DisplayCsvFile.java
@@ -1,17 +1,31 @@
 package Week2;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
+import static java.util.Arrays.*;
+
 public class DisplayCsvFile{
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc=new Scanner(new File("/Users/sanjanaravula/Desktop/Assignments/Student.csv"));
-        sc.useDelimiter(",");
-        while (sc.hasNext())
-        {
-            System.out.print(sc.next());
-        }
-        sc.close();
+        Scanner s=new Scanner(new File("/Users/sanjanaravula/Downloads/student.csv"));
+        String line;
 
+        Student[] stu = new Student[10];
+        for(int i=0;s.hasNextLine();i++){
+            line= s.nextLine();
+            String[] str = line.split(",");
+            String firstName = str[0];
+            String lastName = str[1];
+            float gpa = Float.parseFloat(str[2]);
+            stu[i]= new Student(firstName, lastName, gpa);
+
+        }
+
+        sort(stu);
+
+        for (int i=0;i<10;i++){
+            System.out.println(stu[i]);
+        }
     }
 }
 
---
+++ CoreJava/src/Week2/Student.java
@@ -0,0 +1,24 @@
+package Week2;
+
+public class Student implements Comparable<Student>{
+
+    String fn;
+    String ln;
+    float gpa;
+
+    public Student(String fn, String ln, float gpa) {
+        this.fn = fn;
+        this.ln = ln;
+        this.gpa = gpa;
+    }
+
+    public int compareTo(Student s) {
+        return (this.ln).compareTo(s.ln);
+    }
+
+    @Override
+    public String toString() {
+        return this.fn+"  " +this.ln+"  " +this.gpa;
+    }
+}
+
