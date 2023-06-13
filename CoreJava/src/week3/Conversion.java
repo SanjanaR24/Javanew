0870cffc18c5c2cd7e3077c7f14d8c183828a407
SanjanaR24 <sanjana.reddy2403@gmail.com>
2023-06-13 15:04:39 -0500

    commit

---
+++ CoreJava/src/week3/Conversion.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class Conversion extends Exception{
+    public void conversiontoInt(String s){
+    try {
+        int i = Integer.parseInt(s);
+        System.out.println("Output:"+i);
+    }
+        catch(NumberFormatException ne)
+    {
+        System.out.println("Exception: Number Format Exception");
+       // System.out.println(ne);
+
+    }
+
+}
+}
---
+++ CoreJava/src/week3/Main.java
@@ -0,0 +1,27 @@
+package week3;//write a method that converts given string into an integer. Call the method with different inputs and print the return value.
+// Print exception if it is thrown.
+
+import week3.Conversion;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws MyException {
+        System.out.println("Enter a string");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Conversion c = new Conversion();
+        c.conversiontoInt(s);
+
+        //Custom exception with the same concept
+        System.out.println("Enter a string");
+        //Scanner sc1=new Scanner(System.in);
+        try {
+            String s1 = sc.nextLine();
+            MyException me = new MyException();
+            me.conversionToIntCustom(s1);
+        } catch (MyException me) {
+            System.out.println(me.getMessage());
+        }
+    }
+}
