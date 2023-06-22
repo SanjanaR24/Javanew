b699e06ff0ede40d116597f96d2ea23a0461231b
SanjanaR24 <sanjana.reddy2403@gmail.com>
2023-06-22 15:06:05 -0500

    commit

---
+++ CoreJava/src/Week4/TargetSum.java
@@ -0,0 +1,13 @@
+package Week4;
+
+public class TargetSum {
+    public static void main(String[] args){
+        //Given an array of integers and a target sum,
+        // find two numbers in the array that add up to the target sum. Return their indices using a HashMap.
+        //input: [10,20,30,40,50,60] and target: 100
+        //output: [3,5]
+int[] a=new int[]{10,20,30,40,50,60};
+int target=100;
+
+    }
+}
